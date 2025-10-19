b3356764f59585ec7db7a02356041e4779b7e05e
SouadAlsayed <souadalsayed1804@gmail.com>
2025-10-19 21:27:35 +0300

    feat(profile): enable profile updates using server actions

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -181,7 +181,7 @@ export async function createBooking(newBooking) {
 
 /////////////
 // UPDATE
-
+/*
 // The updatedFields is an object which should ONLY contain the updated data
 export async function updateGuest(id, updatedFields) {
   const { data, error } = await supabase
@@ -200,13 +200,13 @@ export async function updateGuest(id, updatedFields) {
 
 export async function updateBooking(id, updatedFields) {
   const { data, error } = await supabase
-    .from("bookings")
+  .from("bookings")
     .update(updatedFields)
     .eq("id", id)
     .select()
     .single();
 
-  if (error) {
+    if (error) {
     console.error(error);
     throw new Error("Booking could not be updated");
   }
@@ -218,10 +218,12 @@ export async function updateBooking(id, updatedFields) {
 
 export async function deleteBooking(id) {
   const { data, error } = await supabase.from("bookings").delete().eq("id", id);
-
+  
   if (error) {
     console.error(error);
     throw new Error("Booking could not be deleted");
   }
   return data;
 }
+
+*/
